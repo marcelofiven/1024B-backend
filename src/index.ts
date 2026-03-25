58442886d21c7df26eb1bbefa878252865503521
marcelofiven <marceloltlpp@gmail.com>
2026-03-25 11:19:07 -0300

    app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import mysql from 'mysql2/promise';
 import express from 'express'
 const app = express()
+app.use(express.json())
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -21,6 +22,13 @@ app.post("/pessoas", async(req, res) => {
     try {
         //const preparacao = await connection.prepare("select * from pessoa");
         const {id,nome} = req.body
+       //valide se o id e o nome foram passdos corrretamente. (algum valor)
+       //se não for, retorne um status 400 com a mensagem "id ou nome invalidos"
+       //não deixe o codigo executar a parte de baixo quando for inválido.
+        if(!id || !nome){
+            return res.status(400).json({mensagem:"id ou nome invalidos"})
+        }
+       
         const [resultado, campos] =
             await connection.execute(`insert into pessoa values (?,?)`, [id, nome])
         console.log(resultado)
